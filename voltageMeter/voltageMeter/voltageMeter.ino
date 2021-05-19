864f85f01f6e4dd370b8cd92eda94c683e5f5455
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2021-05-19 19:10:21 +0200

    Update voltageMeter.ino

--- voltageMeter/voltageMeter/voltageMeter.ino
+++ voltageMeter/voltageMeter/voltageMeter.ino
@@ -1,8 +1,11 @@
 const int inPin0 = 0;
 const int inPin1 = 1;
 
-void calculatekW(int &sum)
+void calculatekW(float &sum)
 {
+  //kb átlagban 1050 kWh-t termel egy átlagos napelem
+  //számítások sajnos nincsenek meg nekem, de átlagban mondható, hogy 
+  //1650x1650mm a méret, ami 2,72m^2. 
   int valueAt0 = analogRead(inPin0);         
   int valueAt1 = analogRead(inPin1);         
   
@@ -11,7 +14,8 @@ void calculatekW(int &sum)
   float amper = 0.52;
   float kWat0 = (voltsAt0 * amper) / 1000;
   float kWat1 = (voltsAt1 * amper) / 1000;
-  sum = sum + kWat0 + kWat1;
+  float kWat2 = (kWat0 + kWat1) / 2;
+  sum = sum + kWat0 + kWat1 + kWat2;
   delay(500);
 
 
